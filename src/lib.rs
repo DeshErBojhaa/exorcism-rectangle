0923f3cb12a6bc7355ac5fc73ffcf40ce75101a1
tamjidahmed <mail.to.tamjid@gmail.com>
2024-10-22 20:39:17 +0600

    initial implementation
    Signed-off-by: tamjidahmed <mail.to.tamjid@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,71 @@
 pub fn count(lines: &[&str]) -> u32 {
-    todo!("\nDetermine the count of rectangles in the ASCII diagram represented by the following lines:\n{lines:#?}\n.");
+    let mut ans = 0u32;
+    let r = lines.len();
+    if r == 0 {
+        return ans;
+    }
+    let c = lines[0].len();
+    if c < 2 {
+        return 0;
+    }
+
+    for k in 0..r {
+        for l in 0..c {
+            if lines[k].chars().nth(l).unwrap() != '+' {
+                continue;
+            }
+
+            for i in 0..k {
+                for j in 0..l {
+                    if lines[i].chars().nth(j).unwrap() != '+' {
+                        continue;
+                    }
+
+                    if lines[i].chars().nth(l).unwrap() != '+' {
+                        continue;
+                    }
+
+                    if lines[k].chars().nth(j).unwrap() != '+' {
+                        continue;
+                    }
+
+                    // All horizontals are dash
+                    let mut valid = true;
+                    for h in j+1..l {
+                        if lines[i].chars().nth(h).unwrap() != '-' {
+                            valid = false;
+                            break;
+                        }
+                        if lines[k].chars().nth(h).unwrap() != '-' {
+                            valid = false;
+                            break;
+                        }
+                    }
+                    if !valid {
+                        continue;
+                    }
+
+                    // All vertical are |
+                    for v in i+1..k {
+                        if lines[v].chars().nth(j).unwrap() != '|' {
+                            valid = false;
+                            break;
+                        }
+
+                        if lines[v].chars().nth(l).unwrap() != '|' {
+                            valid = false;
+                            break;
+                        }
+                    }
+
+                    if !valid {
+                        continue;
+                    }
+                    println!("Bazinga {}{} -- {}{}", i,j,k,l);
+                    ans += 1;
+                }
+            }
+        }
+    }
+    ans
 }
